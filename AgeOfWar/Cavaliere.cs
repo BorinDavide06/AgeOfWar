f6fb8c9b35d2027691c44051ba9c37d75adef5a1
UTENTE 15 <UTENTE 15@PC15-LAB3>
2023-12-13 09:55:16 +0100

    Aggiungere i file di progetto.

---
+++ AgeOfWar/Cavaliere.cs
@@ -0,0 +1,18 @@
+﻿using AgeOfWar.Personaggi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfWar
+{
+    public class Cavaliere:Personaggio
+    {
+        public Cavaliere(int puntiForza )
+        {
+            PuntiVita = 1000;
+            PuntiForza += puntiForza;
+        }
+    }
+}
